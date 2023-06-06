ee4cbbc629c2a0c7bf87880cd399cd76e2d58938
TheRandomCrafter83 <williams.carl.83@gmail.com>
2023-06-06 18:42:56 -0400

    added more parameters to the CustomComposeTheme class

--- app/src/main/java/com/coderzf1/testtheming/MainActivity.kt
+++ app/src/main/java/com/coderzf1/testtheming/MainActivity.kt
@@ -10,18 +10,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.tooling.preview.Preview
 import com.coderzf1.testtheming.ui.theme.CustomComposeTheme
-import com.coderzf1.testtheming.ui.theme.Pink40
 import com.coderzf1.testtheming.ui.theme.Purple40
-import com.coderzf1.testtheming.ui.theme.PurpleGrey40
 import com.coderzf1.testtheming.ui.theme.TestThemingTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             var testColor by remember { mutableStateOf(Color(Purple40.toArgb()))}
-            TestThemingTheme(customComposeTheme = CustomComposeTheme(
-                primaryColor = testColor)
-            ) {
+            TestThemingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -44,7 +40,7 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Button(onClick = {
-                            testColor = Color.Green
+                            //testColor = Color.Green
                         }) {
                             Text(text = "Click Me")
                         }
@@ -56,3 +52,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/*
+(customComposeTheme = CustomComposeTheme(
+primaryColor = testColor)
+)*/
--- app/src/main/java/com/coderzf1/testtheming/ui/theme/CustomComposeTheme.kt
+++ app/src/main/java/com/coderzf1/testtheming/ui/theme/CustomComposeTheme.kt
@@ -1,22 +1,43 @@
 package com.coderzf1.testtheming.ui.theme
 
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 data class CustomComposeTheme(
-    val primaryColor: Color = Color(0xFF6650a4),
-    val secondaryColor:Color = Color(0xFF625b71),
-    val tertiaryColor:Color = Color(0xFF7D5260),
-    val backgroundColor:Color = Color(0xFFFFFBFE),
-    val surfaceColor:Color = Color(0xFFFFFBFE),
-    val onPrimaryColor:Color = Color.White,
-    val onSecondaryColor:Color = Color.White,
-    val onTertiaryColor:Color = Color.White,
-    val onBackgroundColor:Color = Color(0xFF1C1B1F),
-    val onSurfaceColor:Color = Color(0xFF1C1B1F),
+    val darkTheme:Boolean = false,
+    val primaryColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).primary,
+    val secondaryColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).secondary,
+    val tertiaryColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).tertiary,
+    val backgroundColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).background,
+    val surfaceColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).surface,
+    val onPrimaryColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onPrimary,
+    val onSecondaryColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onSecondary,
+    val onTertiaryColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onTertiary,
+    val onBackgroundColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onBackground,
+    val onSurfaceColor:Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onSurface,
     val statusBarColor:Color = primaryColor,
-    val navigationBarColor:Color = Color.Black
+    val navigationBarColor:Color = Color.Black,
+    val primaryContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).primaryContainer,
+    val onPrimaryContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onPrimaryContainer,
+    val inversePrimaryColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).inversePrimary,
+    val secondaryContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).secondaryContainer,
+    val onSecondaryContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onSecondaryContainer,
+    val tertiaryContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).tertiaryContainer,
+    val onTertiaryContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onTertiaryContainer,
+    val surfaceVariantColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).surfaceVariant,
+    val onSurfaceVariantColor: Color  = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onSurfaceVariant,
+    val surfaceTintColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).surfaceTint,
+    val inverseSurfaceColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).inverseSurface,
+    val inverseOnSurfaceColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).inverseOnSurface,
+    val errorColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).error,
+    val onErrorColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onError,
+    val errorContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).errorContainer,
+    val onErrorContainerColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).onErrorContainer,
+    val outlineColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).outline,
+    val outlineVariantColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).outlineVariant,
+    val scrimColor: Color = (when(darkTheme){true->darkColorScheme() false->lightColorScheme()}).scrim
 ){
     fun asColorScheme(): ColorScheme {
         return lightColorScheme(
@@ -29,7 +50,26 @@ data class CustomComposeTheme(
             onSecondary = onSecondaryColor,
             onTertiary = onTertiaryColor,
             onBackground = onBackgroundColor,
-            onSurface = onSurfaceColor
+            onSurface = onSurfaceColor,
+            primaryContainer = primaryContainerColor,
+            onPrimaryContainer = onPrimaryContainerColor,
+            inversePrimary = inversePrimaryColor,
+            secondaryContainer = secondaryContainerColor,
+            onSecondaryContainer = onSecondaryContainerColor,
+            tertiaryContainer = tertiaryContainerColor,
+            onTertiaryContainer = onTertiaryContainerColor,
+            surfaceVariant = surfaceVariantColor,
+            onSurfaceVariant  = onSurfaceVariantColor,
+            surfaceTint = surfaceTintColor,
+            inverseSurface = inverseSurfaceColor,
+            inverseOnSurface = inverseOnSurfaceColor,
+            error = errorColor,
+            onError = onErrorColor,
+            errorContainer = errorContainerColor,
+            onErrorContainer = onErrorContainerColor,
+            outline = outlineColor,
+            outlineVariant = outlineVariantColor,
+            scrim = scrimColor
         )
     }
 }
